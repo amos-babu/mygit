9492a8a63e86eef04d224a4e9c35f712adafd8a8
amos-babu <amos.babu@yahoo.com>
2025-06-02 00:05:16 +0300

    [any message]

--- app/main.go
+++ app/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -18,19 +21,71 @@ func main() {
 	switch command := os.Args[1]; command {
 	case "init":
 		// Uncomment this block to pass the first stage!
-		
+
 		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
 			if err := os.MkdirAll(dir, 0755); err != nil {
 				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
 			}
 		}
-		
+
 		headFileContents := []byte("ref: refs/heads/main\n")
 		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
 			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
 		}
-		
+
 		fmt.Println("Initialized git directory")
+	case "cat-file":
+		if len(os.Args) != 4 {
+			fmt.Fprintf(os.Stderr, "usage: mygit cat-file -p <object-hash>\n")
+			os.Exit(1)
+		}
+
+		//check third argument is (-p) fourth is valid hash
+		readFlag := os.Args[2]
+		objectHash := os.Args[3]
+
+		if readFlag != "-p" && len(objectHash) != 40 {
+			fmt.Fprintf(os.Stderr, "usage: mygit cat-file -p <object-hash>\n")
+			os.Exit(1)
+		}
+
+		//create filepath
+		dirName := objectHash[0:2]
+		fileName := objectHash[2:]
+		filePath := fmt.Sprintf("./.git/objects/%s/%s", dirName, fileName)
+
+		//read the file
+		fileContents, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
+			os.Exit(1)
+		}
+
+		// decompress the file contents
+		b := bytes.NewReader(fileContents)
+		r, err := zlib.NewReader(b)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error decompressing the file: %s\n", err)
+			os.Exit(1)
+		}
+
+		decompressedData, err := io.ReadAll(r)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading decompressed data: %s\n", err)
+			os.Exit(1)
+		}
+		r.Close()
+
+		// Find the index of the null terminator
+		nullIndex := bytes.IndexByte(decompressedData, 0)
+		if nullIndex == -1 {
+			fmt.Fprintf(os.Stderr, "Invalid object format: missing metadata separator\n")
+			os.Exit(1)
+		}
+
+		// Extract and print the actual content (everything after the null byte)
+		content := decompressedData[nullIndex+1:]
+		fmt.Print(string(content))
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
