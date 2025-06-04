0ce7c10377d988e77d00c0e151ed664f892a373f
amos-babu <amos.babu@yahoo.com>
2025-06-04 13:01:10 +0300

    first commit

--- app/main.go
+++ app/main.go
@@ -11,7 +11,7 @@ import (
 // Usage: your_program.sh <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintf(os.Stderr, "Logs from your program will appear here!\n")
+	// fmt.Fprintf(os.Stderr, "Logs from your program will appear here!\n")
 
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
@@ -20,75 +20,85 @@ func main() {
 
 	switch command := os.Args[1]; command {
 	case "init":
-		// Uncomment this block to pass the first stage!
-
-		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
-			if err := os.MkdirAll(dir, 0755); err != nil {
-				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
-			}
+		if err := initCommand(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
-
-		headFileContents := []byte("ref: refs/heads/main\n")
-		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
-		}
-
 		fmt.Println("Initialized git directory")
+
 	case "cat-file":
-		if len(os.Args) != 4 {
+		if len(os.Args) != 4 || os.Args[2] != "-p" {
 			fmt.Fprintf(os.Stderr, "usage: mygit cat-file -p <object-hash>\n")
 			os.Exit(1)
 		}
 
-		//check third argument is (-p) fourth is valid hash
-		readFlag := os.Args[2]
-		objectHash := os.Args[3]
-
-		if readFlag != "-p" && len(objectHash) != 40 {
-			fmt.Fprintf(os.Stderr, "usage: mygit cat-file -p <object-hash>\n")
-			os.Exit(1)
-		}
-
-		//create filepath
-		dirName := objectHash[0:2]
-		fileName := objectHash[2:]
-		filePath := fmt.Sprintf("./.git/objects/%s/%s", dirName, fileName)
-
-		//read the file
-		fileContents, err := os.ReadFile(filePath)
+		content, err := catFileCommand(os.Args[3])
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
+			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
 
-		// decompress the file contents
-		b := bytes.NewReader(fileContents)
-		r, err := zlib.NewReader(b)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error decompressing the file: %s\n", err)
-			os.Exit(1)
-		}
-
-		decompressedData, err := io.ReadAll(r)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading decompressed data: %s\n", err)
-			os.Exit(1)
-		}
-		r.Close()
-
-		// Find the index of the null terminator
-		nullIndex := bytes.IndexByte(decompressedData, 0)
-		if nullIndex == -1 {
-			fmt.Fprintf(os.Stderr, "Invalid object format: missing metadata separator\n")
-			os.Exit(1)
-		}
-
-		// Extract and print the actual content (everything after the null byte)
-		content := decompressedData[nullIndex+1:]
 		fmt.Print(string(content))
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
 	}
+}
+
+func initCommand() error {
+	for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return fmt.Errorf("Error creating directory: %s: %w\n", dir, err)
+		}
+	}
+
+	headFileContents := []byte("ref: refs/heads/main\n")
+	if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
+		return fmt.Errorf("Error writing .git/HEAD: %w\n", err)
+	}
+
+	return nil
+}
+
+func catFileCommand(hash string) ([]byte, error) {
+	objectHash := hash
+
+	if len(objectHash) != 40 {
+		fmt.Fprintf(os.Stderr, "usage: mygit cat-file -p <object-hash>\n")
+		os.Exit(1)
+	}
+
+	dirName := objectHash[0:2]
+	fileName := objectHash[2:]
+	filePath := fmt.Sprintf("./.git/objects/%s/%s", dirName, fileName)
+
+	fileContents, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
+		os.Exit(1)
+	}
+
+	b := bytes.NewReader(fileContents)
+	r, err := zlib.NewReader(b)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error decompressing the file: %s\n", err)
+		os.Exit(1)
+	}
+
+	decompressedData, err := io.ReadAll(r)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading decompressed data: %s\n", err)
+		os.Exit(1)
+	}
+	r.Close()
+
+	nullIndex := bytes.IndexByte(decompressedData, 0)
+	if nullIndex == -1 {
+		fmt.Fprintf(os.Stderr, "Invalid object format: missing metadata separator\n")
+		os.Exit(1)
+	}
+
+	content := decompressedData[nullIndex+1:]
+	return content, nil
 }
